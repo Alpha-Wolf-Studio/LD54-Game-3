8fea19463cfcb3732b9a214575f3002c038fc9c1
Gonzalo Montero <gonmon654@gmail.com>
2023-09-29 22:38:16 -0300

    Added localization tools

---
+++ Assets/Code/Utility/AutomaticTextLocalizer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class AutomaticTextLocalizer : MonoBehaviour
+{
+    TextMeshProUGUI textField;
+
+    public string key;
+    
+    private void Start()
+    {
+        textField = GetComponent<TextMeshProUGUI>();
+
+        string s = Loc.ReplaceKey(key); 
+
+        textField.text = s;
+    }
+}

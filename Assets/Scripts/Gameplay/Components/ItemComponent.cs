5e9d5d2d0ccd381826d9c771e86b0a1cfa63c33b
Adrian Sgro <Owinmowe@gmail.com>
2023-09-30 03:20:44 -0300

    Add basic interaction interfaces with placeholder tests for FlipFlop/Return states

---
+++ Assets/Scripts/Gameplay/Components/ItemComponent.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+using Gameplay.Interfaces;
+
+namespace Gameplay.Components
+{
+    [RequireComponent(typeof(Collider2D))]
+    public class ItemComponent : MonoBehaviour, IInteractable
+    {
+        public event Action OnInteract;
+        public void Interact()
+        {
+            OnInteract?.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/Gameplay/Controls/InteractionControl.cs
@@ -0,0 +1,39 @@
+using System;
+using Gameplay.Interfaces;
+using UnityEngine;
+
+namespace Gameplay.Controls
+{
+    public class InteractionControl : MonoBehaviour
+    {
+        
+        private Camera _camera;
+        private readonly Collider2D[] _collider2Ds = new Collider2D[10];
+        
+        private void Start()
+        {
+            _camera = Camera.main;
+        }
+
+        private void Update()
+        {
+            if (!Input.GetMouseButtonDown(0)) return;
+            
+            Vector3 origin = _camera.ScreenToWorldPoint(Input.mousePosition);
+            origin.z = 0;
+
+            int size = Physics2D.OverlapPointNonAlloc(origin, _collider2Ds);
+
+            if (size <= 0) return;
+            
+            for (int i = 0; i < size; i++)
+            {
+                if (_collider2Ds[i].TryGetComponent(out IInteractable interactable))
+                {
+                    interactable.Interact();
+                    return;
+                }
+            }
+        }
+    }
+}
